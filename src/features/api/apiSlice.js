c40c1dcc6f99626713f9e76d24ec1cec8070da67
anisayser <anisayser@gmail.com>
2023-02-25 10:52:58 +0600

    Live Server link Added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,7 +5,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8000/api/v1"
+        // baseUrl: "http://localhost:8000/api/v1"
+        baseUrl: "https://trendy-tees.onrender.com/api/v1"
     }),
     tagTypes: ["Cart"],
     endpoints: (builder) => ({})
